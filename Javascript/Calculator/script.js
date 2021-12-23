07f9719bd4e414dadd6e8db6bb62ee5395ac852d
Ravikant Tyagi <dhruvasharma1989@gmail.com>
2021-12-23 08:10:54 +0530

    Add files via upload

---
+++ Javascript/Calculator/script.js
@@ -0,0 +1,49 @@
+window.addEventListener("load", bindEvents);
+
+function bindEvents() {
+    var addBtn = document.querySelector("#add");
+    var subBtn = document.querySelector("#sub");
+    var divBtn = document.querySelector("#div");
+    var mulBtn = document.querySelector("#mul");
+
+    addBtn.addEventListener("click", add);
+    subBtn.addEventListener("click", sub);
+    divBtn.addEventListener("click", div);
+    mulBtn.addEventListener("click", mul);
+}
+
+function add() {
+    var fnum = document.querySelector("#fnum").value;
+    var snum = document.querySelector("#snum").value;
+    // we need to perform type casting
+    // convert one type of data into another
+    var result = parseInt(fnum) + parseInt(snum);
+    console.log(result);
+}
+
+function sub() {
+    var fnum = document.querySelector("#fnum").value;
+    var snum = document.querySelector("#snum").value;
+    // we need to perform type casting
+    // convert one type of data into another
+    var result = parseInt(fnum) - parseInt(snum);
+    console.log(result);
+}
+
+function div() {
+    var fnum = document.querySelector("#fnum").value;
+    var snum = document.querySelector("#snum").value;
+    // we need to perform type casting
+    // convert one type of data into another
+    var result = parseInt(fnum) / parseInt(snum);
+    console.log(result);
+}
+
+function mul() {
+    var fnum = document.querySelector("#fnum").value;
+    var snum = document.querySelector("#snum").value;
+    // we need to perform type casting
+    // convert one type of data into another
+    var result = parseInt(fnum) * parseInt(snum);
+    console.log(result);
+}
