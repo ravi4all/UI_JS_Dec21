c9857d84e93d036201c175ab4c1eec386bb6713a
Ravikant Tyagi <dhruvasharma1989@gmail.com>
2022-01-13 08:17:50 +0530

    Add files via upload

--- Javascript/TicTacToe/custom.js
+++ Javascript/TicTacToe/custom.js
@@ -4,6 +4,11 @@ var userChoice;
 var userChoiceSelector;
 var cpuChoice;
 var btns;
+
+var winning_combinations = [[0,1,2],[3,4,5],[6,7,8],
+                            [0,3,6],[1,4,7],[2,5,8],
+                            [0,4,8],[2,4,6]];
+
 function initEvents() {
     userChoiceSelector = document.querySelector("#choice");
     var playBtn = document.querySelector("#play-btn");
@@ -31,6 +36,7 @@ function userMove() {
     var btn = this;
     btn.innerHTML = userChoice;
     btn.setAttribute('disabled','true');
+    checkWinner(userChoice, btn.title);
 
     setTimeout(function(){
         cpuMove();
@@ -50,6 +56,21 @@ function cpuMove() {
     else {
         btn.innerHTML = cpuChoice;
         btn.setAttribute('disabled','true');
+        checkWinner(cpuChoice, cpuIndex);
     }
     
+}
+
+
+function checkWinner(choice, pos) {
+    for(var i = 0; i < winning_combinations.length; i++) {
+        for(var j = 0; j < winning_combinations[i].length; j++) {
+            if (pos == winning_combinations[i][j]) {
+                winning_combinations[i][j] = choice;
+            }
+        }
+    }
+
+    // Write loop to check winner
+
 }
